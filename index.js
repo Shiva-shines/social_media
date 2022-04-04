c23b205e6797c94cbfd1b9406fb8cb4af8a22dd7
shivam <shivamsingh07092000@gmail.com>
2022-04-04 16:44:39 +0530

    Create view for home

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require ('express');
 const port = 8000;
 const app = express();
 app.use('/' , require('./routes'));
-app.set('view engine', ejs);
+app.set('view engine', 'ejs');
 app.set('views', './views');
 
 
