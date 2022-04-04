292a94c68790c0cc2a15f855e5f1eea23c8d44c8
shivam <shivamsingh07092000@gmail.com>
2022-04-04 15:37:03 +0530

    first commit

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,5 @@
 const express =require('express');
-const router = require('.');
-const ruoter = express.Router();
+const router = express.Router();
 
 const usersController = require('../controllers/users_controller');
 router.get('/profile',usersController.profile);
