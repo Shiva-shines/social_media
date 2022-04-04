8408d5a2273521f1a411ca29c449b9aa11818430
shivam <shivamsingh07092000@gmail.com>
2022-04-04 14:47:19 +0530

    intial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require ('express');
+const port = 8000;
+const app = express();
+app.use('/' , require('./routes/index'));
+
+app.listen(port, function(err){
+    if(err){
+        console.log(`error in running server: ${err}`);
+        return;
+    }
+    console.log(`server is up and running on port: ${port}`);
+    
+});
