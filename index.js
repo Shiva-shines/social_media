e7477934b80e63df79bb7fb447e508676e437409
shivam <shivamsingh07092000@gmail.com>
2022-04-05 18:49:56 +0530

    create logIn and signIn page and render it

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require ('express');
+const cookieParser = require('cookie-parser');
 const port = 8000;
 const expressLayouts = require('express-ejs-layouts');
 const db = require('./config/mongoose');
--- routes/users.js
+++ routes/users.js
@@ -3,4 +3,6 @@ const router = express.Router();
 
 const usersController = require('../controllers/users_controller');
 router.get('/profile',usersController.profile);
+router.get('/sign-in',usersController.signIn);
+router.get('/log-in',usersController.logIn);
 module.exports=router;
